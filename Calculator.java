8f8b6acfece62d2aed97fad9f65e46b86e776c11
AnushkaHiremath13 <anushkahiremath13@gmail.com>
2025-04-29 10:45:46 +0530

    save

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,7 @@ public class Calculator {
     public static void main(String[] args) {
         double n1 = 20;
         double n2 = 5;
+        double n3 = 27;
 
         double add = n1 + n2;
         double sub = n1 - n2;
@@ -10,12 +11,15 @@ public class Calculator {
 
         System.out.println("addition: " + add);
         System.out.println("substraction: " + sub);
-        System.out.println("multiplication: " + mul);
+        System.out.println("mult: " + mul);
         
         if (n2 != 0) {
             System.out.println("division: " + div);
         } else {
             System.out.println("division: Cannot divide by zero.");
         }
+        System.out.println("Power: " + Math.pow(n1, n2));
+        System.out.println("Cube Root: " + Math.cbrt(n3));
+        System.out.println("Square Root: " + Math.sqrt(n1));
     }
 }
